47ecbe58b40436cf7874acf5aab4271f75987706
jakobwalter <jakob.walter@outlook.de>
2022-09-02 17:15:33 +0200

    Added documentation to classical permutation test

--- R/ClassicPermTest.R
+++ R/ClassicPermTest.R
@@ -1,8 +1,24 @@
+#' Classic Permutation Test for RNA-Seq Data
+#'
+#' Computes p-values using a classic permutation test based on the absolute
+#' difference in means. The data is normalized using edgeR.
+#' @param X A vector encoded as a factor with two levels. 
+#' The levels encode the two different classes that are to be tested for differential expression.
+#' @param Y An array or dataframe 
+#' @keyword absolute difference in means permutation test RNA-Seq
+#' @importFrom base sum
+#' @export
+#' @examples
+#' mean(1:3)
+#' \dontrun{ mean(1:1e99) }
+
+
+
 ClassicPermTest <- function(X, Y, nPerm){
   ## Normalize Data Using EdgeR
   
   ### Compute Normalization Factors
-  d <- edgeR::DGEList(counts = X)
+  d <- edgeR::DGEList(counts = Y)
   d <- edgeR::calcNormFactors(d)
   
   ### Normalize Data
